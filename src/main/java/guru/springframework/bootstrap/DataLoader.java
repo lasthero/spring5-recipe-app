47448bb5b0e5d6a19bc9c6a6db52999dc481272b
lasthero <chihho.chou@gmail.com>
2019-07-29 16:33:55 -0400

    Add another recipe on bootstrap   - update to bootsrap 4 css   - add an image

--- src/main/java/guru/springframework/bootstrap/DataLoader.java
+++ src/main/java/guru/springframework/bootstrap/DataLoader.java
@@ -41,7 +41,14 @@ public class DataLoader implements ApplicationListener<ContextRefreshedEvent> {
       + "For a very quick guacamole just take a 1/4 cup of salsa and mix it in with your mashed avocados.\n"
       + "\n"
       + "Feel free to experiment! One classic Mexican guacamole has pomegranate seeds and chunks of peaches in it (a Diana Kennedy favorite). Try guacamole with added pineapple, mango, or strawberries (see our Strawberry Guacamole).";
-
+  private final String CHICKEN_TACO_DIRECTIONS = "1 Prepare a gas or charcoal grill for medium-high, direct heat.\n"
+      + "2 Make the marinade and coat the chicken: In a large bowl, stir together the chili powder, oregano, cumin, sugar, salt, garlic and orange zest. Stir in the orange juice and olive oil to make a loose paste. Add the chicken to the bowl and toss to coat all over.\n"
+      + "Set aside to marinate while the grill heats and you prepare the rest of the toppings.\n"
+      + "3 Grill the chicken: Grill the chicken for 3 to 4 minutes per side, or until a thermometer inserted into the thickest part of the meat registers 165F. Transfer to a plate and rest for 5 minutes.\n"
+      + "4 Warm the tortillas: Place each tortilla on the grill or on a hot, dry skillet over medium-high heat. As soon as you see pockets of the air start to puff up in the tortilla, turn it with tongs and heat for a few seconds on the other side.\n"
+      + "Wrap warmed tortillas in a tea towel to keep them warm until serving.\n"
+      + "5 Assemble the tacos: Slice the chicken into strips. On each tortilla, place a small handful of arugula. Top with chicken slices, sliced avocado, radishes, tomatoes, and onion slices. Drizzle with the thinned sour cream. Serve with lime wedges.\n";
+  private final String CHICKEN_TACO_NOTES = "Look for ancho chile powder with the Mexican ingredients at your grocery store, or buy it online. (If you can't find ancho chili powder, you replace the ancho chili, the oregano, and the cumin with 2 1/2 tablespoons regular chili powder, though the flavor won't be quite the same.)";
   @Autowired
   public DataLoader(RecipeRepository recipeRepository,
       CategoryRepository categoryRepository,
@@ -72,17 +79,105 @@ public class DataLoader implements ApplicationListener<ContextRefreshedEvent> {
     perfectGuacamole.setCookTime(0);
     perfectGuacamole.setServings(4);
     addPerfectGuacamoleIngredients(perfectGuacamole);
-    perfectGuacamole.setImage(GetPerfectGuacamoleImage());
+    perfectGuacamole.setImage(GetRecipeImage("guacamole.jpg"));
     perfectGuacamole.setDirections(PERFECT_GUACAMOLE_DIRECTIONS);
     Notes perfectGuacamoleNotes = new Notes();
     perfectGuacamoleNotes.setRecipeNotes(PERFECT_GUACAMOLE_NOTES);
     perfectGuacamole.setNotes(perfectGuacamoleNotes);
 
-
     recipeRepository.save(perfectGuacamole);
-
+    recipeRepository.save(ChickenTacoRecipe());
   }
 
+  private Recipe ChickenTacoRecipe(){
+    Recipe chickenTacoRecipe = new Recipe();
+    chickenTacoRecipe.setDescription("Spicy Grilled Chicken Tacos");
+    chickenTacoRecipe.setSource("Simply Recipes");
+    chickenTacoRecipe.setUrl("https://www.simplyrecipes.com/recipes/spicy_grilled_chicken_tacos/");
+    chickenTacoRecipe.setDifficulty(Difficulty.KIND_OF_HARD);
+    chickenTacoRecipe.setCategories(new HashSet<>(Arrays.asList(categoryRepository.findByDescription("Mexican").get(), categoryRepository.findByDescription("Fast Food").get())));
+    chickenTacoRecipe.setPrepTime(20);
+    chickenTacoRecipe.setCookTime(15);
+    chickenTacoRecipe.setServings(6);
+
+    Ingredient chiliPower = new Ingredient();
+    chiliPower.setDescription("Ancho Chili Powder");
+    chiliPower.setAmount(new BigDecimal(2));
+    chiliPower.setUnitOfMeasure(unitOfMeasureRepository.findByDescription("Teaspoon").get());
+    chiliPower.setRecipe(chickenTacoRecipe);
+    chickenTacoRecipe.addIngredient(chiliPower);
+
+    Ingredient oregano = new Ingredient();
+    oregano.setDescription("Dried Oregano");
+    oregano.setAmount(new BigDecimal(1));
+    oregano.setUnitOfMeasure(unitOfMeasureRepository.findByDescription("Teaspoon").get());
+    oregano.setRecipe(chickenTacoRecipe);
+    chickenTacoRecipe.addIngredient(oregano);
+
+    Ingredient cumin = new Ingredient();
+    cumin.setDescription("Dried Cumin");
+    cumin.setAmount(new BigDecimal(1));
+    cumin.setUnitOfMeasure(unitOfMeasureRepository.findByDescription("Teaspoon").get());
+    cumin.setRecipe(chickenTacoRecipe);
+    chickenTacoRecipe.addIngredient(cumin);
+
+    Ingredient sugar = new Ingredient();
+    sugar.setDescription("Sugar");
+    sugar.setAmount(new BigDecimal(1));
+    sugar.setUnitOfMeasure(unitOfMeasureRepository.findByDescription("Teaspoon").get());
+    sugar.setRecipe(chickenTacoRecipe);
+    chickenTacoRecipe.addIngredient(sugar);
+
+    Ingredient salt = new Ingredient();
+    salt.setDescription("Salt");
+    salt.setAmount(new BigDecimal(0.5));
+    salt.setUnitOfMeasure(unitOfMeasureRepository.findByDescription("Teaspoon").get());
+    salt.setRecipe(chickenTacoRecipe);
+    chickenTacoRecipe.addIngredient(salt);
+
+    Ingredient garlic = new Ingredient();
+    garlic.setDescription("Garlic");
+    garlic.setAmount(new BigDecimal(1));
+    garlic.setUnitOfMeasure(unitOfMeasureRepository.findByDescription("Count").get());
+    garlic.setRecipe(chickenTacoRecipe);
+    chickenTacoRecipe.addIngredient(garlic);
+
+    Ingredient orangeZest = new Ingredient();
+    orangeZest.setDescription("Orange Zest");
+    orangeZest.setAmount(new BigDecimal(1));
+    orangeZest.setUnitOfMeasure(unitOfMeasureRepository.findByDescription("Tablespoon").get());
+    orangeZest.setRecipe(chickenTacoRecipe);
+    chickenTacoRecipe.addIngredient(orangeZest);
+
+    Ingredient orangeJuice= new Ingredient();
+    orangeJuice.setDescription("Orange Juice");
+    orangeJuice.setAmount(new BigDecimal(3));
+    orangeJuice.setUnitOfMeasure(unitOfMeasureRepository.findByDescription("Tablespoon").get());
+    orangeJuice.setRecipe(chickenTacoRecipe);
+    chickenTacoRecipe.addIngredient(orangeJuice);
+
+    Ingredient oliveOil= new Ingredient();
+    oliveOil.setDescription("Olive Oil");
+    oliveOil.setAmount(new BigDecimal(2));
+    oliveOil.setUnitOfMeasure(unitOfMeasureRepository.findByDescription("Tablespoon").get());
+    oliveOil.setRecipe(chickenTacoRecipe);
+    chickenTacoRecipe.addIngredient(oliveOil);
+
+    Ingredient chickenThighs= new Ingredient();
+    chickenThighs.setDescription("Boneless Chicken Thighs");
+    chickenThighs.setAmount(new BigDecimal(6));
+    chickenThighs.setUnitOfMeasure(unitOfMeasureRepository.findByDescription("Count").get());
+    chickenThighs.setRecipe(chickenTacoRecipe);
+    chickenTacoRecipe.addIngredient(chickenThighs);
+
+    chickenTacoRecipe.setImage(GetRecipeImage("grilled_chicken_tacos.jpg"));
+    chickenTacoRecipe.setDirections(CHICKEN_TACO_DIRECTIONS);
+    Notes notes = new Notes();
+    notes.setRecipeNotes(CHICKEN_TACO_NOTES);
+    chickenTacoRecipe.setNotes(notes);
+
+    return chickenTacoRecipe;
+  }
   private void addPerfectGuacamoleIngredients(Recipe perfectGuacamoleRecipe) {
 
     Ingredient ripeAvocados = new Ingredient();
@@ -143,9 +238,9 @@ public class DataLoader implements ApplicationListener<ContextRefreshedEvent> {
 
   }
 
-  private Byte[] GetPerfectGuacamoleImage() {
+  private Byte[] GetRecipeImage(String fileName) {
     try {
-      BufferedImage inputImage = ImageIO.read(new File(System.getProperty("user.dir") + "/src/main/resources/static/images/guacamole.jpg"));
+      BufferedImage inputImage = ImageIO.read(new File(System.getProperty("user.dir") + "/src/main/resources/static/images/"+ fileName));
       ByteArrayOutputStream content = new ByteArrayOutputStream();
       ImageIO.write(inputImage, "jpg", content);
 
@@ -156,4 +251,5 @@ public class DataLoader implements ApplicationListener<ContextRefreshedEvent> {
     }
     return null;
   }
+
 }
