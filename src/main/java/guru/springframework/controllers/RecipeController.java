f82d2c5e11e3d586a2ab300ba7e027643046b22a
lasthero <chihho.chou@gmail.com>
2019-10-08 15:58:22 -0400

    Add a custom NotFoundException view  - use ExceptionHandler annotation

--- src/main/java/guru/springframework/controllers/RecipeController.java
+++ src/main/java/guru/springframework/controllers/RecipeController.java
@@ -1,16 +1,23 @@
 package guru.springframework.controllers;
 
 import guru.springframework.commands.RecipeCommand;
+import guru.springframework.exceptions.NotFoundException;
 import guru.springframework.services.RecipeService;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.servlet.ModelAndView;
 
+@Slf4j
 @Controller
 public class RecipeController {
 
@@ -55,6 +62,15 @@ public class RecipeController {
     RecipeCommand savedCommand = recipeService.saveRecipeCommand(command);
 
     return "redirect:/recipe/" + savedCommand.getId() + "/show";
+  }
+
+  @ResponseStatus(HttpStatus.NOT_FOUND)
+  @ExceptionHandler(NotFoundException.class)
+  public ModelAndView handleNotFound() {
+    log.error("Handling not found exception");
+    ModelAndView modelAndView = new ModelAndView();
+    modelAndView.setViewName("404error");
+    return modelAndView;
   }
 
 }
